757cfe3776ddbddd4ecabbf170297685ef023427
Daniel Pinto <cayan@programmer.net>
2023-04-15 12:54:41 -0300

    Add cards and modals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,124 +1,140 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import { Page, LegacyCard, Button, Icon, Modal } from '@shopify/polaris';
+import { CircleInformationMajor } from "@shopify/polaris-icons";
+import { useState } from 'react';
+
+export const items = [
+  {
+    title: "First item",
+    modalTitle: "First item details",
+    modalDescription: "This is the first item",
+  },
+  {
+    title: "Second item",
+    modalTitle: "Second item details",
+    modalDescription: "This is the second item",
+  },
+  {
+    title: "Third item",
+    modalTitle: "Third item details",
+    modalDescription: "This is the third item",
+  },
+  {
+    title: "Fourth item",
+    modalTitle: "Fourth item details",
+    modalDescription: "This is the fourth item",
+  },
+  {
+    title: "Fifth item",
+    modalTitle: "Fifth item details",
+    modalDescription: "This is the fifth item",
+  },
+  {
+    title: "Sixth item",
+    modalTitle: "Sixth item details",
+    modalDescription: "This is the sixth item",
+  },
+  {
+    title: "Seventh item",
+    modalTitle: "Seventh item details",
+    modalDescription: "This is the seventh item",
+  },
+  {
+    title: "Eighth item",
+    modalTitle: "Eighth item details",
+    modalDescription: "This is the eighth item",
+  },
+  {
+    title: "Ninth item",
+    modalTitle: "Ninth item details",
+    modalDescription: "This is the ninth item",
+  },
+  {
+    title: "Tenth item",
+    modalTitle: "Tenth item details",
+    modalDescription: "This is the tenth item",
+  },
+];
+
+export const InfoModal = ({
+  modalOpen,
+  handleCloseModal,
+  modalTitle,
+  modalDescription,
+}: {
+  modalOpen: boolean;
+  handleCloseModal: () => void;
+  modalTitle: string;
+  modalDescription: string;
+}) => {
+  return (
+    <Modal
+      open={modalOpen}
+      onClose={handleCloseModal}
+      title={modalTitle}
+      primaryAction={{
+        content: "OK",
+        onAction: handleCloseModal,
+      }}
+    >
+      <Modal.Section>
+        <p>{modalDescription}</p>
+      </Modal.Section>
+    </Modal>
+  );
+};
 
 export default function Home() {
+  const InfoIcon = () => (
+    <Icon color="subdued" source={CircleInformationMajor} />
+  );
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalProps, setModalProps] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleOpenModal = (
+    title: string,
+    description: string,
+  ) => {
+    setModalProps({
+      title,
+      description,
+    });
+
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Page title="Example app">
+      {items.map((item, index) => (
+        <LegacyCard key={index} title={item.title} sectioned>
+          <p>{item.modalDescription}</p>
+          <Button
+            plain
+            icon=<InfoIcon />
+            onClick={() =>
+              handleOpenModal(
+                item.modalTitle,
+                item.modalDescription,
+              )
+            }
+          ></Button>
+        </LegacyCard>
+      ))}
+      <InfoModal
+        modalOpen={isModalOpen}
+        handleCloseModal={handleCloseModal}
+        modalTitle={modalProps.title}
+        modalDescription={modalProps.description}
+      />
+    </Page>
   )
 }
